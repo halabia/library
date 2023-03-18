c61ac2a2b97ec073469899732f1d2ef4397461cb
Alanjim Halabia <alanjim.halabia@hotmail.com>
2023-03-18 13:15:03 -0400

    feat: display cards with user prompts

--- javascript.js
+++ javascript.js
@@ -1,50 +1,39 @@
-let title = prompt("Enter the book title")
-let author = prompt("Enter the book author")
-let pages = parseInt(prompt("Enter the book pages"))
-let read = prompt("Have you read the book")
+// set initial variables
+const container = document.querySelector('#container');
+var myLibrary = [];
+var count = 0;
 
+// book object constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-}
-Book.prototype.info = function () {
-    return "Book title:\n" + title + "\nBook author:\n" + author + "\nBook pages:\n" + pages + "\nHave you read it:\n" + read
-}
-const hobbit = new Book("hobbit", "jk", "24", "yes")
-let myLibrary = [hobbit];
-//   myLibrary.push(hobbit);
-
-
-function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-
-    var toAdd = document.createDocumentFragment();
-
-    for (let i = 0; i < myLibrary.length; i++) {
-        // const div = document.createElement('div');   
-        var newDiv = document.createElement('p');
-        newDiv.id = 'r' + i;
-        newDiv.className = 'ansbox';
-        newDiv.textContent = myLibrary[i].info()
-        toAdd.appendChild(newDiv);
-
-        // let container = document.querySelector('#container');
-
-        // let p = document.createElement('p');
-        // p.style.cssText = 'color: white; background: black;'; 
-        // p.textContent = myLibrary[i].info()
-        // container.appendChild(p);
-        // // container.setAttribute('style', 'color: blue; background: white;'); 
-        // console.log(myLibrary[i])
-        // // console.log(newBook.info())
+    this.info = function() {
+        return "Book title:\n" + title + "\nBook author:\n" + author + "\nBook pages:\n" + pages + "\nHave you read it:\n" + read
     }
-    //   document.appendChild(toAdd);
-    document.getElementById('container').appendChild(toAdd);
 }
 
-addBookToLibrary(title, author, pages, read)
+// creates book item on screen
+let createBook = (count) => {
+        const content = document.createElement('div');
+        content.classList.add("book-card")
+        content.innerHTML = myLibrary[count].info()
+        container.append(content)
+        // console.log(content.innerHTML)
+}
 
+// triggers user prompt to enter book info everytime the button is clicked, adding items to library and increasing count
+const newBookButton = document.querySelector('#newbie')
+newBookButton.addEventListener('click', () => {
+    let title = prompt("What's the name of the book?")
+    let author = prompt("What's the name of the author?")
+    let pages = parseInt(prompt("How many pages are in the book?"))
+    let read = prompt("Have you read the book? Please enter yes or no.")
 
+    const newBook = new Book(title, author, pages, read)
+    myLibrary.push(newBook)
+
+    createBook(count)
+    count++
+});
